3ae2c128e6767acb01579325dae6a01df8e3aeb9
iluiza06 <isadoraluiza551@gmail.com>
2025-03-22 23:02:05 -0300

    estilizacao da tela de envio de arquivo

--- Front-end/src/components/CsvUploader.vue
+++ Front-end/src/components/CsvUploader.vue
@@ -28,14 +28,49 @@ const handleFileUpload = (ev) => {
 </script>
 
 <template>
-    <div>
-        <input type="file" accept=".csv" @change="handleFileUpload" />
-        <CsvTable :csvData="csvData" />
-    </div>
+
+    <img src="/group-6.png" alt="Decor Left" class="decor-left" />
+    <img src="/logo.png" alt="Logo Bonsae" class="logo" />
+
+    <label class="file-button">
+        Escolha um arquivo CSV
+        <input type="file" accept=".csv" @change="handleFileUpload" hidden />
+
+    </label>
+
+
 </template>
 
 <style scoped>
-input[type="file"] {
-    margin-bottom: 20px;
+.file-button {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    position: relative;
+    bottom: 25rem;
+    left: 30rem;
+    margin: 0;
+    width: 190px;
+    display: inline-block;
+    text-align: center;
+    border: 2px solid #139AFE;
+    border-radius: 25px;
+    padding: 10px 20px;
+    cursor: pointer;
+    color: #011738;
+    background-color: none;
+}
+
+
+.decor-left {
+    height: 44rem;
+    margin: 0;
+    width: 15rem;
+}
+
+.logo {
+    width: 350px;
+    height: 125px;
+    position: absolute;
+    left: 39rem;
+    top: 5rem;
 }
 </style>
